2f7f5adc385f9f4913454db431d5fc51ddc5120c
lantemir <temiros@mail.ru>
2021-12-28 19:17:23 +0600

    first commit

---
+++ app.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div v-for="post in posts" :key="post.id">
+      <p>{{ post.id }}</p>
+      <p>{{ post.header }}</p>
+      <p>{{ post.content }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  async mounted() {
+    let req = await axios.get("http://127.0.0.1:8000/api/posts");
+    this.posts = req.data;
+  },
+};
+</script>
+
+<style scoped>
+.my-text {
+  color: brown;
+}
+</style>
